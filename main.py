bab664f44656daac7a6dd66c22269cfb64d32da0
unknown <buronfosseetienne@gmail.com>
2021-03-24 13:03:00 +0100

    majuscule

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ local_ip = socket.gethostbyname(hostname)  # ipDeLaMachine
 
 print(psutil.cpu_percent(interval=1))
 
-bucket = os.getenv("BUCKET")
-org = os.getenv("ORG")
+BUCKET = os.getenv("BUCKET")
+ORG = os.getenv("ORG")
 TOKEN = os.getenv("INFLUXDB_TOKEN")
 # Store the URL of your InfluxDB instance
 URL = os.getenv("URL")
@@ -24,7 +24,7 @@ URL = os.getenv("URL")
 client = influxdb_client.InfluxDBClient(
     url=URL,
     token=TOKEN,
-    org=org
+    org=ORG
 )
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
@@ -35,9 +35,9 @@ def test():
     cpuPercent = psutil.cpu_percent(interval=1)
     point = influxdb_client.Point("sensors").field("Battery Level", batteryLevel.percent).tag("host_name",
                                                                                               local_ip + "")
-    write_api.write(bucket=bucket, org=org, record=point)
+    write_api.write(bucket=BUCKET, org=ORG, record=point)
     point = influxdb_client.Point("cpu").field("CPU Percent", cpuPercent).tag("host_name", local_ip + "")
-    write_api.write(bucket=bucket, org=org, record=point)
+    write_api.write(bucket=BUCKET, org=ORG, record=point)
     time.sleep(1)
     print(batteryLevel.percent)
     print(cpuPercent)
