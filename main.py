b4282349c4a4f5aa54e778aa372dbaf7e53a119d
Axel Pélerin <arcanynka@gmail.com>
2021-03-24 14:51:54 +0100

    Put in a try/except battery level to make it work on non laptop computer. Replace local_ip by mac adress

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import influxdb_client
-from influxdb_client.client.write_api import SYNCHRONOUS
 import psutil
 import time
-from dotenv import load_dotenv
-import os
 import socket
+from os import getenv
+from influxdb_client.client.write_api import SYNCHRONOUS
+from getmac import get_mac_address as gma
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -13,13 +14,11 @@ batteryLevel = psutil.sensors_battery()
 hostname = socket.gethostname()  # nomDeLaMachine
 local_ip = socket.gethostbyname(hostname)  # ipDeLaMachine
 
-print(psutil.cpu_percent(interval=1))
-
-BUCKET = os.getenv("BUCKET")
-ORG = os.getenv("ORG")
-TOKEN = os.getenv("INFLUXDB_TOKEN")
+BUCKET = getenv("BUCKET")
+ORG = getenv("ORG")
+TOKEN = getenv("INFLUXDB_TOKEN")
 # Store the URL of your InfluxDB instance
-URL = os.getenv("URL")
+URL = getenv("URL")
 
 client = influxdb_client.InfluxDBClient(
     url=URL,
@@ -31,16 +30,17 @@ write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
 def test():
-    batteryLevel = psutil.sensors_battery()
-    cpuPercent = psutil.cpu_percent(interval=1)
-    point = influxdb_client.Point("sensors").field("Battery Level", batteryLevel.percent).tag("host_name",
-                                                                                              local_ip + "")
-    write_api.write(bucket=BUCKET, org=ORG, record=point)
-    point = influxdb_client.Point("cpu").field("CPU Percent", cpuPercent).tag("host_name", local_ip + "")
+    battery_level = psutil.sensors_battery()
+    cpu_percent = psutil.cpu_percent(interval=1)
+    try:
+        point = influxdb_client.Point("sensors").field("Battery Level", battery_level.percent).tag("host_name",
+                                                                                                   gma() + "")
+        write_api.write(bucket=BUCKET, org=ORG, record=point)
+    except AttributeError:
+        pass
+    point = influxdb_client.Point("cpu").field("CPU Percent", cpu_percent).tag("host_name", gma() + "")
     write_api.write(bucket=BUCKET, org=ORG, record=point)
     time.sleep(1)
-    print(batteryLevel.percent)
-    print(cpuPercent)
     test()
 
 
