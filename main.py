6b49db281628056acc7e1cbe84cde480a4b68a97
unknown <buronfosseetienne@gmail.com>
2021-03-24 12:40:17 +0100

    tests influxDB

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 import psutil
 import time
+from dotenv import load_dotenv
+import os
+import socket
 
+load_dotenv()
 
 batteryLevel = psutil.sensors_battery()
 
-print(batteryLevel.percent)
+hostname = socket.gethostname() #nomDeLaMachine
+local_ip = socket.gethostbyname(hostname) #ipDeLaMachine
+
+print(psutil.cpu_percent(interval=1))
 
 bucket = "etienne.buronfosse's Bucket"
 org = "etienne.buronfosse@edu.itescia.fr"
-token = "8ir7A_gX_SEHX872iiXVGCcEGQNhlXLGSPSKSjjj-ndk9X10JfgDq5NWF091v9GqKuj4fFM-CclJ5mTk3rLHzg=="
+TOKEN = os.getenv("INFLUXDB_TOKEN")
 # Store the URL of your InfluxDB instance
-url="https://westeurope-1.azure.cloud2.influxdata.com"
+URL=os.getenv("URL")
 
 client = influxdb_client.InfluxDBClient(
-    url=url,
-    token=token,
+    url=URL,
+    token=TOKEN,
     org=org
 )
 
@@ -24,10 +31,14 @@ write_api = client.write_api(write_options=SYNCHRONOUS)
 
 def test():
     batteryLevel = psutil.sensors_battery()
-    point = influxdb_client.Point("sensors").tag("host_name", "ordi_etienne").field("BatteryLevel", batteryLevel.percent)
+    cpuPercent = psutil.cpu_percent(interval=1)
+    point = influxdb_client.Point("sensors").field("Battery Level", batteryLevel.percent).tag("host_name", local_ip + "")
     write_api.write(bucket=bucket, org=org, record=point)
-    time.sleep(2) 
+    point = influxdb_client.Point("cpu").field("CPU Percent", cpuPercent).tag("host_name", local_ip + "")
+    write_api.write(bucket=bucket, org=org, record=point)
+    time.sleep(1) 
     print(batteryLevel.percent)
+    print(cpuPercent)
     test()
 
 test()
